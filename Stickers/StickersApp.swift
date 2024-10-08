51d753637142623aa462d557cf7f4f8505360acc
rfrancodev <francorfs@icloud.com>
2024-10-08 11:28:44 -0300

    Initial Commit

---
+++ Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by Rafael Franco on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
